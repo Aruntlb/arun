c5dc4ba41032bd2af91363720814b5cad4f53057
arun <aruntlb12@gmail.com>
2022-08-30 09:32:33 +0530

    comparative

---
+++ src/sample1/Comparative.java
@@ -0,0 +1,12 @@
+package sample1;
+
+public class Comparative {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int a=5,b=6;
+		System.out.println(a>=b);
+
+	}
+
+}
